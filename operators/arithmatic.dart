4ec6d70de4a8994b225d6f74c67c55e95cc7a2cd
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-22 22:36:23 +0530

    Create arithmatic.dart

---
+++ operators/arithmatic.dart
@@ -0,0 +1,16 @@
+void main(){
+  int x=10;
+  int y=8;
+  //sum
+  print(x+y);
+  //diff
+  print(x-y);
+  //division
+  print(x/y);
+  //multiplication
+  print(x*y);
+  //integer division
+  print(x ~/y);
+  //modulas
+  print(x%y);
+}
