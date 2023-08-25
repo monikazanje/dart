e5d0dc970abf6655a8812dc47816cf0da11d0bb6
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-25 22:54:03 +0530

    Update checkNum.dart

--- ifElse/checkNum.dart
+++ ifElse/checkNum.dart
@@ -1,8 +1,24 @@
+/* write a dart program take a number and print wheather it is less than 10 or greater than 10;
+inpute : var =5;
+output: 5 is less than 10
+
+inpute : var =16;
+output: 16 is greater than 10
+*/
+
 void main(){
- // int num=16;
+ 
   int num=5;
   if(num<10){
     print("5 is less than 10");
+  }
+  else{
+    print("it  is greater than 10");
+  }
+
+  num=16;
+  if(num<10){
+    print("16 is less than 10");
   }
   else{
     print("16 is greater than 10");
