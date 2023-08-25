21fc79132fc9a4c9401399a9638da641c781bcbe
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-25 22:51:39 +0530

    Update evenOdd.dart

--- ifElse/evenOdd.dart
+++ ifElse/evenOdd.dart
@@ -1,8 +1,24 @@
+/* Write a program to Check if a number is even or odd.
+inpute : var =10;
+output: 10 is an even no
+
+inpute : var =37;
+output: 37 is an odd no
+*/
+
 void main(){
-  var num=37;
-  //var num=10;
+  
+  var num=10;
   if(num%2==0){
     print("10 is even number");
+  }
+  else{
+    print(" it is not even number");
+  }
+
+  num=37;
+  if(num%2==0){
+    print("is even number");
   }
   else{
     print("37 is odd number");
