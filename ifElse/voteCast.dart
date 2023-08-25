41933d1fd091258a717f5a6158b81003b74fdb66
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-25 22:55:57 +0530

    Update voteCast.dart

--- ifElse/voteCast.dart
+++ ifElse/voteCast.dart
@@ -1,7 +1,22 @@
+/* Write a program to determine if the user can cast a vote or not
+(a person with age above 18 can cast a vote)
+input :age =18
+output : you can cast a vote 
+
+inpute: age = 14
+output : you can't cast a vote */
 void main(){
-  //int age=18;
-  int age=14;
+  int age=18;
+  
   if(age>=18){
+    print("you can cast vote");
+  }
+  else{
+    print( "you can't cast a vote");
+  }
+
+   age=14;
+   if(age>=18){
     print("you can cast vote");
   }
   else{
