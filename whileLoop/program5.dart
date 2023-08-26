146edf628735e5d5029bb2aeae847ba9b017deca
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-26 19:38:05 +0530

    Create program5.dart

---
+++ whileLoop/program5.dart
@@ -0,0 +1,13 @@
+//write a program to print the square of even digits between 40 to 50
+
+import "dart:io";
+void main(){
+  int num=40;
+  while(num<=50){
+    if(num%2==0){
+      
+      stdout.write("${num*num} " );
+    }
+    num++;
+  }
+}
