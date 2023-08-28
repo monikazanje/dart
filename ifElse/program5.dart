c884309827d39a8a1f75962282f23e6c9b26999c
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-28 21:48:18 +0530

    Create program5.dart

---
+++ ifElse/program5.dart
@@ -0,0 +1,19 @@
+/*
+write a program to check if a character is a vowel or consonant
+input: var = "d";
+output : d is a consonant
+
+input: var = "a";
+output: a is a vowels
+ */
+void main(){
+   var str="A";
+   if(str=="A"||str=="E"||str=="I"||str=="O"||str=="U"){
+    print("$str is a vowel");
+   }
+   else{
+     print("$str is a consonant");
+   }
+
+   
+}
