d4864fc44845fd7fbcb7fffec3f161652a6d3dac
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-29 19:10:18 +0530

    Create program2.dart

---
+++ switchCase/program2.dart
@@ -0,0 +1,13 @@
+//not only int and float is work also string is work in dart
+
+void main(){
+  var x="monday";
+  switch(x){
+    case 1:
+    print("one");
+    case 2:
+    print("two");
+    default :
+    print("no match");
+  }
+}
