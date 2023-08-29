eed86bc895c57a0c34015e638b4fd29802f1e3f3
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-29 19:11:56 +0530

    Create program5.dart

---
+++ switchCase/program5.dart
@@ -0,0 +1,21 @@
+void main(){
+  int x=3;
+  do{
+  print("1:c");
+  print("2:cpp");
+  print("3:java");
+  print("4:dart");
+  switch(x){
+    case 1:
+    print("1972");
+    case 2:
+    print("1976");
+    case 3 :
+    print("1995");
+    case 4:
+    print("2013");
+    default:
+    print("no match");
+  }
+  }while(false);
+}
