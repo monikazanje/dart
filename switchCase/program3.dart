0632dd047b9a7d98323c17e93bbd4b8eeb3c45a4
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-29 19:11:07 +0530

    Create program3.dart

---
+++ switchCase/program3.dart
@@ -0,0 +1,19 @@
+
+
+void main(){
+  var x=1;
+  switch(x){
+    case 1:
+    print("one");
+    case 2:
+    print("two");
+    case 1:
+    print("three");
+    case 3:
+    print("four");
+    default :
+    print("no match");
+
+
+  }
+}
