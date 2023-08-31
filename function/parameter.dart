47f8317ae3a98fd837100538a57a7ea49bf6319f
monika <120088441+monikazanje@users.noreply.github.com>
2023-08-31 23:25:56 +0530

    Create parameter.dart

---
+++ function/parameter.dart
@@ -0,0 +1,11 @@
+void fun(String name,[double sal=10.8]){
+  print("in function");
+  print(name);
+  print(sal);
+}
+void main(){
+  print("start main");
+  fun("monika");
+  fun("monika",20.5);
+  print("end main");
+}
