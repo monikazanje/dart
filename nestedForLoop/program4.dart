ce17c1d0387035b5e71b1e346ca7124180d4ad07
monika <120088441+monikazanje@users.noreply.github.com>
2023-09-17 20:30:53 +0530

    Create program4.dart

---
+++ nestedForLoop/program4.dart
@@ -0,0 +1,14 @@
+//write  a program to print following pattern
+//1A 1A 1A
+//1A 1A 1A
+//1A 1A 1A
+import "dart:io";
+ void main(){
+  for(int i=1;i<=3;i++){
+    for(int j=1;j<=3;j++){
+      stdout.write("1A ");
+    }
+    print(" ");
+  }
+
+ }
