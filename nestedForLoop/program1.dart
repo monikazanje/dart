6115ccbb8fade6bc53b0bed9565d981bd7640780
monika <120088441+monikazanje@users.noreply.github.com>
2023-09-17 20:29:30 +0530

    Create program1.dart

---
+++ nestedForLoop/program1.dart
@@ -0,0 +1,17 @@
+//write  a program to print following pattern
+//C2W C2W C2W
+//C2W C2W C2W
+//C2W C2W C2W
+
+
+import 'dart:io';
+void main(){
+
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      stdout.write(" C2W ");
+    }
+    print(" ");
+  }
+  
+}
