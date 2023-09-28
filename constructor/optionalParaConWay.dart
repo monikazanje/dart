b67046d77566c339f3c6963ea489b2f8e29393ee
monika <120088441+monikazanje@users.noreply.github.com>
2023-09-28 11:33:38 +0530

    Create optionalParaConWay.dart

---
+++ constructor/optionalParaConWay.dart
@@ -0,0 +1,15 @@
+class company{
+  int? comId;
+  String? compName;
+  company(this. comId,[ this.compName="Binecaps"]);
+  void printdata(){
+    print(comId);
+    print(compName);
+  }
+}
+void main(){
+  company obj1=new company(101,"veritas");
+  company obj2=new company(102);
+  obj1.printdata();
+  obj2.printdata();
+}
