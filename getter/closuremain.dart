f17c4befe4dfcb079c351d060d81b76323d0310a
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-02 23:51:06 +0530

    Create closuremain.dart

---
+++ getter/closuremain.dart
@@ -0,0 +1,12 @@
+import "closureClass.dart";
+void main(){
+  demo obj=new demo(10,"monika",1.3);
+  print(obj.getX);
+  print(obj.getName);
+  print(obj.getSal);
+
+  obj.display();
+  print(obj.display);
+
+
+}
