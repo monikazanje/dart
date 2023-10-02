3bd7387ef4f88fb11dac6979a2787a4aa7997895
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-02 23:41:55 +0530

    Create programClassWay3.dart

---
+++ getter/programClassWay3.dart
@@ -0,0 +1,12 @@
+class demo{
+  int? _x;
+  String? str;
+  double _sal;
+
+  demo(this._x,this.str,this._sal);
+
+  int? get getX => _x;
+  String?  get getName => str;
+  double? get getSal =>_sal;
+
+}
