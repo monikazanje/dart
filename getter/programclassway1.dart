1915daadc38e9d2f7f58a7038734b4f0ab16c3d4
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-02 23:34:46 +0530

    Create programclassway1.dart

---
+++ getter/programclassway1.dart
@@ -0,0 +1,18 @@
+class demo{
+  int? _x;
+  String? str;
+  double _sal;
+
+  demo(this._x,this.str,this._sal);
+
+  int? getX(){
+    return _x;
+  }
+  String? getName(){
+    return str;
+  }
+  double? getSal(){
+    return _sal;
+  }
+
+}
