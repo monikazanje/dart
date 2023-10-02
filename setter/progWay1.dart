925870aeba5b4e2b6dd22483a02a731e430e2539
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-03 00:04:22 +0530

    Create progWay1.dart

---
+++ setter/progWay1.dart
@@ -0,0 +1,20 @@
+
+  import "progClassWay1.dart";
+void main(){
+  player obj=new player(10,"monika",1.3);
+  obj.display();
+
+  obj.setX(15);
+  obj.setSal(2.3);
+  obj.setstr("yash");
+  obj.display();
+
+  
+}
+
+// 10
+// monika
+// 1.3
+// 15
+// monika
+// 2.3
