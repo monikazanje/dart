3641097788731fd2e5b4a51e2297c6a32c9e51ae
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-07 01:36:52 +0530

    Create program3.dart

---
+++ pattern/program3.dart
@@ -0,0 +1,15 @@
+//write a program to print following code
+ // 14 14 14 14 
+ // 15 15 15 15 
+ // 16 16 16 16 
+ // 17 17 17 17 
+
+import "dart:io";
+void main(){
+  for(int i=1;i<=4;i++){
+    for(int j=1;j<=4;j++){
+      stdout.write(" ${i+13}");
+    }
+    print(" ");
+  }
+}
