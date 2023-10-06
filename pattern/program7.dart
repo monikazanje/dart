5878e383c1a866ae0ccd08adfeaadc261c9e09f1
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-07 01:51:51 +0530

    Create program7.dart

---
+++ pattern/program7.dart
@@ -0,0 +1,15 @@
+//write a program to print following code
+// D D D D  
+// D D D D  
+// D D D D  
+// D D D D  
+
+import 'dart:io';
+void main(){
+  for(int i=1;i<=4;i++){
+    for(int j=1;j<=4;j++){
+      stdout.write("D ");
+    }
+    print(" ");
+  }
+}
