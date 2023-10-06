9a67dc97ed2e7f1cb8aeb875afd7c8e51fcd1871
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-07 01:42:47 +0530

    Create program4.dart

---
+++ pattern/program4.dart
@@ -0,0 +1,16 @@
+//write a program to print following code
+ // 1A 1A 1A 
+ // 1A 1A 1A 
+ // 1A 1A 1A 
+
+
+import "dart:io";
+
+void main(){
+  for(int i=1;i<=3;i++){
+    for(int j=1;j<=3;j++){
+      stdout.write(" 1A");
+    }
+    print(" ");
+  }
+}
