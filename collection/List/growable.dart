f45aaeed555b696bbd98ea8d63bda224c13288e7
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-26 22:50:30 +0530

    Create growable.dart

---
+++ collection/List/growable.dart
@@ -0,0 +1,12 @@
+void main(){
+  List <String> name=List.empty(growable: true);
+  print(name);
+  name.add("monika");
+  name.add("yash");
+  print(name);
+}
+
+
+// output:
+// []
+// [monika, yash]
