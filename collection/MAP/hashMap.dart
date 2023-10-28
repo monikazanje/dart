8f6b8052b12ae048428904998835b033ae9f8c24
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-28 15:10:12 +0530

    Update hashMap.dart

--- collection/MAP/hashMap.dart
+++ collection/MAP/hashMap.dart
@@ -2,9 +2,21 @@ import "dart:collection";
 
 void main(){
   var player=HashMap();
+  //first method to add in hashMap
   player[18]="virat";
   print(player);
+
+  //addAll
+  player.addAll({45:"rohit"});
+  print(player);
+
+  //addEntries
+  player.addEntries({7:"MSD",1:"KLRahul"}.entries);
+  print(player);
+
 }
 
 // output:
 // {18: virat}
+// {18: virat, 45: rohit}
+// {1: KLRahul, 18: virat, 45: rohit, 7: MSD}
