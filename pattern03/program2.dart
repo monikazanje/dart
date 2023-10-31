a45dc42033305efeaa96fa89b27b36216df6f062
monika <120088441+monikazanje@users.noreply.github.com>
2023-10-31 23:12:02 +0530

    Create program2.dart

---
+++ pattern03/program2.dart
@@ -0,0 +1,18 @@
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+import 'dart:io';
+void main(){
+  int? n=4;
+
+  for(int i=1;i<=4;i++){
+    for(int j=1;j<=i;j++){
+      stdout.write("$j  ");
+
+     
+    }
+    print("");
+
+}
+}
