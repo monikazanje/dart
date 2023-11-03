746707547d281be349edb17f25ce38974d4cf610
monika <120088441+monikazanje@users.noreply.github.com>
2023-11-03 23:52:15 +0530

    Create contains.dart

---
+++ collection/Iterable/contains.dart
@@ -0,0 +1,14 @@
+void main(){
+  var players=["Rohit","virat","shubman","KLRahul","shreyash","hardik"];
+  var retval=players.contains("virat");
+  var retval1=players.contains("shami");
+  var retval2=players.contains("Rohit");
+  print(retval);
+  print(retval1);
+  print(retval2);
+}
+
+// output:
+// true
+// false
+// true
