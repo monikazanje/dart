06067a63f4bd2eb5845e913d5ed4ae28aca50412
monika <120088441+monikazanje@users.noreply.github.com>
2023-11-04 14:40:55 +0530

    Create join.dart

---
+++ collection/Iterable/join.dart
@@ -0,0 +1,8 @@
+void main(){
+  var players=["Rohit","virat","shubman","KLRahul","shreyash","hardik"];
+  var retval=players.join("->");
+  print(retval);
+}
+
+// output:
+// Rohit->virat->shubman->KLRahul->shreyash->hardik
