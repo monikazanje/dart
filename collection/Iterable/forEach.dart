c4200e7cfa7500414570994c4d2a771970c98ef6
monika <120088441+monikazanje@users.noreply.github.com>
2023-11-04 14:38:01 +0530

    Create forEach.dart

---
+++ collection/Iterable/forEach.dart
@@ -0,0 +1,13 @@
+void main(){
+  var players=["Rohit","virat","shubman","KLRahul","shreyash","hardik"];
+  var retval=players.forEach((element)=>print(element));
+ 
+}
+
+// output:
+// Rohit
+// virat
+// shubman
+// KLRahul
+// shreyash
+// hardik
