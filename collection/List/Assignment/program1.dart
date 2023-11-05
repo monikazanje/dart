4b7057e3de01d655516a11f89a2a3fbf5bbacadd
monika <120088441+monikazanje@users.noreply.github.com>
2023-11-05 14:55:59 +0530

    Create program1.dart

---
+++ collection/List/Assignment/program1.dart
@@ -0,0 +1,24 @@
+import 'dart:collection';
+import 'dart:io';
+
+void main(){
+  List num=<int>[];
+  print("enter length of list");
+  var n=int.parse(stdin.readLineSync()!);
+  int count=0;
+  print("elments of the list is");
+
+  for(int i=0;i<n;i++){
+    var input=int.parse(stdin.readLineSync()!);
+    num.add(input);
+  }
+   print(num);
+  
+  for(int i=0 ;i<n;i++){
+    if(num[i] %2==0){
+      count++;
+    }
+  }
+  print(count);
+
+}
