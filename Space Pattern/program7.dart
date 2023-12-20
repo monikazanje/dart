5c254c17b250bed66cd87f9ee05b44e22f17a640
monika <120088441+monikazanje@users.noreply.github.com>
2023-12-20 23:34:52 +0530

    Update program7.dart

--- Space Pattern/program7.dart
+++ Space Pattern/program7.dart
@@ -7,7 +7,7 @@ import "dart:io";
 void main() {
   int n = 3;
   int a = 1;
-  int temp = a;
+
   for (int i = 1; i <= 4; i++) {
     a = i * i;
     for (int j = 1; j <= n; j++) {
@@ -17,8 +17,7 @@ void main() {
       stdout.write("$a");
       a = a - i;
     }
-    a = temp + 1;
-    temp++;
+    
     n--;
     print("");
   }
